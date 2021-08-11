99bf6eab34d44e631da7b52aef3a8d0991bb7062
George Kollias <286415+geokollias@users.noreply.github.com>
2021-08-12 00:37:43 +0300

    Typo

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -34,7 +34,7 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 		return nil, err
 	}
 	indexFile, err := os.OpenFile(
-		path.Join(dir, fmt.Sprintf("%d,%s", baseOffset, ".index")),
+		path.Join(dir, fmt.Sprintf("%d%s", baseOffset, ".index")),
 		os.O_RDWR|os.O_CREATE,
 		0644,
 	)

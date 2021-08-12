d8ec1d45df6dc3f07d333d421c382afeae41d570
George Kollias <286415+geokollias@users.noreply.github.com>
2021-08-12 13:16:14 +0300

    [chapter 4] grpc server

--- internal/log/log_test.go
+++ internal/log/log_test.go
@@ -50,7 +50,8 @@ func testAppendRead(t *testing.T, log *Log) {
 func testOutOfRangeErr(t *testing.T, log *Log) {
 	read, err := log.Read(0)
 	require.Nil(t, read)
-	require.Error(t, err)
+	apiErr := err.(api.ErrOffsetOutOfRange)
+	require.Equal(t, uint64(0), apiErr.Offset)
 }
 
 func testInitExisting(t *testing.T, o *Log) {
